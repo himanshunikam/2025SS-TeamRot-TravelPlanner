be85434bc89afd6ab2dd4206029bbc362bff054f
Thomas Antony <t.antony@oth-aw.de>
2025-06-02 14:18:08 +0200

    git config testing

--- client/src/components/CityPage.jsx
+++ client/src/components/CityPage.jsx
@@ -133,7 +133,7 @@ const CityPage = () => {
             </div>
 
 
-
+#testin
 {/* City Description */}
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="bg-white rounded-lg shadow-md p-6 mb-12">
